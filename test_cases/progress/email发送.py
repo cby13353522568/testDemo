bdb89a8bc2d341666c9e84619e42e5b210c82be0
cby13353522568 <835871924@qq.com>
2022-03-02 15:34:57 +0800

    #短信邮件收发

--- test_cases/progress/email发送.py
+++ test_cases/progress/email发送.py
@@ -1,5 +1,6 @@
 import smtplib   # 发邮件用的库
 from email.mime.text import MIMEText   # 发邮件文本用的库
+from email.mime.multipart import MIMEMultipart  # 带附件
 
 
 # 163邮箱授权密码：NMHFAUOUDPRPWDOC   cby13353522568@163.com   cby@689689
@@ -10,14 +11,22 @@ SMTPServer = "smtp.163.com"
 sender = "cby13353522568@163.com"
 # 发送者授权密码
 senderPwd = "NMHFAUOUDPRPWDOC"
-# 发送内容
-msg = "SMTP mail test"
-senderMessage = MIMEText(msg)  # 将str转化为邮件文本
+# 发送内容-正文
+senderMessage = MIMEMultipart()
+
+msgMain = MIMEText("SMTP mail test")  # 将str转化为邮件文本
+senderMessage.attach(msgMain)
 # 标题
 senderMessage['Subject'] = '我想你了'
 # 发送者
 senderMessage['From'] = sender
-
+# 附件
+with open('report.html', 'r', encoding='utf-8') as fp:
+    attach = fp.read()
+msgAttach = MIMEText(attach, 'text')
+msgAttach["Content-Type"] = 'application/octet-stream'
+msgAttach["Content-Disposition"] = 'attachment; filename="report.html"'
+senderMessage.attach(msgAttach)
 
 # 创建SMTP服务器，25是端口
 mailServer = smtplib.SMTP(SMTPServer, 25)
